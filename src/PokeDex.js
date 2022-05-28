b67d5e4f3797ba4f8dc18763fcd5625a9f00dcab
DHULIPALLA <ajaybabu.dhulipalla@dxc.com>
2022-05-28 16:07:08 +0530

    Modifed

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -8,6 +8,12 @@ function PokeDex() {
   const [pokemons, setPokemons] = useState([]);
   const [pokemonDetail, setPokemonDetail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [name,setData]=
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((Response) => setPokemons(Response.pokemon));
+  },[]);
 
   const customStyles = {
     content: {
@@ -26,15 +32,19 @@ function PokeDex() {
   if (!isLoading && pokemons.length === 0) {
     return (
       <div>
+
         <header className="App-header">
           <h1>Welcome to pokedex !</h1>
           <h2>Requirement:</h2>
           <ul>
             <li>
-              Call this api:https://pokeapi.co/api/v2/pokemon to get pokedex, and show a list of pokemon name.
+              Call this api:https://pokeapi.co/api/v2/pokemon to get pokedex,
+              and show a list of pokemon name.
             </li>
             <li>Implement React Loading and show it during API call</li>
-            <li>when hover on the list item , change the item color to yellow.</li>
+            <li>
+              when hover on the list item , change the item color to yellow.
+            </li>
             <li>when clicked the list item, show the modal below</li>
             <li>
               Add a search bar on top of the bar for searching, search will run
@@ -42,7 +52,10 @@ function PokeDex() {
             </li>
             <li>Implement sorting and pagingation</li>
             <li>Commit your codes after done</li>
-            <li>If you do more than expected (E.g redesign the page / create a chat feature at the bottom right). it would be good.</li>
+            <li>
+              If you do more than expected (E.g redesign the page / create a
+              chat feature at the bottom right). it would be good.
+            </li>
           </ul>
         </header>
       </div>
@@ -64,6 +77,9 @@ function PokeDex() {
           <>
             <h1>Welcome to pokedex !</h1>
             <b>Implement Pokedex list here</b>
+            <div>
+              {pokemons.map(item=><li>{item.name}</li>)}
+            </div>
           </>
         )}
       </header>
@@ -85,7 +101,10 @@ function PokeDex() {
                 required in tabular format
               </li>
               <li>Create a bar chart based on the stats above</li>
-              <li>Create a  buttton to download the information generated in this modal as pdf. (images and chart must be included)</li>
+              <li>
+                Create a buttton to download the information generated in this
+                modal as pdf. (images and chart must be included)
+              </li>
             </ul>
           </div>
         </Modal>
